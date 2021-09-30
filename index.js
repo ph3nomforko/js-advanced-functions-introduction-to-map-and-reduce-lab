b39acb0ecbb73f77a7a689529506fde866e7ccc4
Codi Kraus <codi.kraus@gmail.com>
2021-09-30 09:39:49 -0500

    commit tree db2f4da42e0519fbf49b50e24ae9c70437c690c4

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    for (const num in sourceArray) {
+       const negative = sourceArray[num] * -1
+       newArray.push(negative)
+    }
+    return newArray
+ }
+
+ function mapToNoChange(sourceArray) {
+    let newArray = [...sourceArray]
+    return newArray
+ }
+
+ function mapToDouble(sourceArray) {
+    let newArray = []
+    for (const num in sourceArray) {
+       const doubled = sourceArray[num] * 2
+       newArray.push(doubled)
+    }
+    return newArray
+ }
+
+ function mapToSquare(sourceArray) {
+    let newArray = []
+    for (const num in sourceArray) {
+       const doubled = sourceArray[num] ** 2
+       newArray.push(doubled)
+    }
+    return newArray
+ }
+
+// Reduce
+
+ function reduceToTotal(sourceArray, startingPoint = 0) {
+    let total = startingPoint
+    for (let i = 0; i < sourceArray.length; i++ ) {
+      total = total + sourceArray[i]
+    }
+    return total
+ }
+
+ function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (!sourceArray[i]) return false
+      }
+      return true
+ }
+
+ function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (sourceArray[i]) return true
+      }
+      return false
+ }
